1157f5a5327f9542c2e52c2d8d06efb09631b296
Alexander de Sousa <alexander.jose.de.sousa@gmail.com>
2021-07-18 12:16:33 +0200

    Updated dependencies

--- mix.exs
+++ mix.exs
@@ -1,17 +1,18 @@
 defmodule ExReg.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "1.0.0"
   @root "https://github.com/gmtprime/exreg"
 
   def project do
     [
-      name: "ExReg",
       app: :exreg,
+      name: "ExReg",
       version: @version,
-      elixir: "~> 1.8",
+      elixir: "~> 1.12",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      dialyzer: dialyzer(),
       package: package(),
       docs: docs(),
       deps: deps()
@@ -23,17 +24,35 @@ defmodule ExReg.Mixfile do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.19", only: :dev},
-      {:credo, "~> 1.0", only: :dev}
+      {:ex_doc, "~> 0.24", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false}
     ]
+  end
+
+  def application do
+    [
+      extra_applications: [],
+      mod: {ExReg, []}
+    ]
+  end
+
+  def dialyzer do
+    [ptl_file: {:no_warn, "priv/plts/exreg"}]
   end
 
   #########
   # Package
 
+  defp description do
+    """
+    A simple process name registry that uses pg (formerly known as pg2).
+    """
+  end
+
   defp package do
     [
-      description: "A simple process name registry that uses pg2",
+      description: description(),
       files: ["lib", "mix.exs", "README.md", "CHANGELOG.md"],
       maintainers: ["Alexander de Sousa"],
       licenses: ["MIT"],
